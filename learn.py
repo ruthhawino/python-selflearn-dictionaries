3bd8d767ab4c654c27ecba87d18b23ddfa901b13
RedietZeraiHadera <redietzeraihadderra@gmail.com>
2023-04-22 19:46:28 +0300

    first commit

---
+++ learn.py
@@ -0,0 +1,40 @@
+
+#//Create an empty dictionary called dog
+dog={}
+#/Add name, color, breed, legs, age to the dog dictionary
+dog['name']='Rex'
+dog ['color']='brown'
+dog['breed']='German shepherd'
+dog['legs']=4
+dog['age']=3
+
+print(dog)
+
+#Create a student dictionary and add first_name, last_name, gender, age,
+#marital status, skills, country, city and address as keys for the dictionary
+#Get the length of the student dictionary.
+
+
+
+# create a dictionary with student information
+student = {
+    'first_name': 'Ruth',
+    'last_name': 'Alex',
+    'gender': 'Female',
+    'age': 20,
+    'marital_status': 'Single',
+    'skills': ['Python', 'Javascript', 'HTML', 'CSS'],
+    'country': 'Kenya',
+    'city': 'Nairobi',
+    'address': '149-KorongoroRoad.'
+}
+
+# get length of the dictionary
+length = len(student)
+
+# print the dictionary and its length
+print(student)
+print('Length of the student dictionary:', length)
+
+
+
